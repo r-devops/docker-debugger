604526cccdbbf115c676aac997170d854d3a335b
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2024-02-20 05:12:42 +0530

    Changes overall

--- cfdi/profile.sh
+++ cfdi/profile.sh
@@ -1,1 +1,6 @@
 export PATH=$PATH:/opt/kafka/bin 
+alias git='git -c http.sslVerify=false'
+alias ls='ls --color'
+alias k='kubectl'
+export PS1="[ \[\e[1;31m\]\u\[\e[m\]@\[\e[1;33m\]ec2-connect\[\e[m\] \[\e[1;36m\]\w\[\e[m\] ]\\$ "
+
